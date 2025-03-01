202d7c23072910a235a0078747bd567b5b7bcb97
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-02-28 22:52:26 -0500

    add special_pythagorean_triplet method and corresponding tests

---
+++ tests/special_pythagorean_triplet_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative '../project_euler/special_pythagorean_triplet'
+
+class SpecialPythagoreanTripletTest < Minitest::Test
+  def test_special_pythagorean_triplet
+    assert_equal 60, special_pythagorean_triplet(12)
+    assert_equal 31875000, special_pythagorean_triplet(1000)
+    assert_nil special_pythagorean_triplet(11)
+  end
+end
