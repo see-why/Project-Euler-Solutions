5e5ef4d422c4e951fba1102913c33598f9d40b5c
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-02-28 20:41:25 -0500

    add largest_prime_factor method to calculate the largest prime factor of a given limit

---
+++ largest_prime_factor.rb
@@ -0,0 +1,31 @@
+def largest_prime_factor(limit)
+    number = 2
+    prime = 2
+    while number <= limit do
+        if limit % number == 0
+            limit /= number
+            puts "number: #{number}"
+            puts "limit: #{limit}"
+            prime = isPrime(number) ? number : prime
+        end
+        number += 1
+
+    end
+    prime
+end
+
+def isPrime(n)
+    if n <= 1
+        return false
+    end
+    for i in 2..Math.sqrt(n)
+        if n % i == 0
+            return false
+        end
+    end
+    return true
+end
+
+
+puts largest_prime_factor(600851475143)
+puts largest_prime_factor(10)
