881c5110cc2f14a1857567ae5a174b562262e6d7
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-02 01:37:24 -0500

    add amicable_numbers method and corresponding tests

---
+++ project_euler/amicable_numbers.rb
@@ -0,0 +1,32 @@
+def sum_of_proper_divisors(n)
+  return 1 if n <= 1
+
+  sum = 1
+  sqrt = Math.sqrt(n).to_i
+
+  (2..sqrt).each do |i|
+    if n % i == 0
+      sum += i
+      sum += n / i if i != sqrt
+    end
+  end
+
+  sum
+end
+
+def amicable_numbers(limit)
+  amicable_sum = 0
+
+  (2...limit).each do |n|
+    sum1 = sum_of_proper_divisors(n)
+    # sum1 <= n not allowed so we don't count the same number twice
+    next if sum1 >= limit || sum1 <= n
+
+    sum2 = sum_of_proper_divisors(sum1)
+    if sum2 == n
+      amicable_sum += n + sum1
+    end
+  end
+
+  amicable_sum
+end
---
+++ tests/amicable_numbers_test.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require_relative "../project_euler/amicable_numbers"
+
+class AmicableNumbersTest < Minitest::Test
+  def test_same_result_for_n_and_n_plus_one
+    n = 220
+    result_n = amicable_numbers(n)
+    result_n_plus_one = amicable_numbers(n + 1)
+    assert_equal result_n, result_n_plus_one, "Expected amicable_numbers(#{n}) to equal amicable_numbers(#{n + 1})"
+  end
+
+  def test_sum_of_amicable_numbers_up_to_10000
+    expected_sum = 31626
+    assert_equal expected_sum, amicable_numbers(10000), "Expected sum of amicable numbers up to 10000 to be #{expected_sum}"
+  end
+
+  def test_multiple_pairs_of_amicable_numbers
+    expected_sum = 504
+    assert_equal expected_sum, amicable_numbers(300), "Expected sum of amicable numbers up to 300 to be #{expected_sum}"
+  end
+end
