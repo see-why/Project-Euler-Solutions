ed2dab85ed2c258aa954b073ca9c7a938ad972be
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-02 19:22:50 -0500

    optimize quadratic_primes method by caching prime checks and precomputing b primes

--- project_euler/quadratic_primes.rb
+++ project_euler/quadratic_primes.rb
@@ -3,14 +3,20 @@ require_relative "largest_prime_factor"
 def quadratic_primes(a_limit, b_limit)
   max_primes = 0
   max_coefficients = [0, 0]
+  primes_cache = {}
+
+  b_primes = (-b_limit).upto(b_limit).select { |n| is_prime(n) }
 
   (-a_limit + 1).upto(a_limit - 1) do |a|
-    (-b_limit).upto(b_limit) do |b|
+    b_primes.each do |b|
       next if b == 0 || a == 0
       n = 0
       loop do
         number = n**2 + (a * n) + b
-        break if number < 2 || !is_prime(number)
+        break if number < 2
+
+        is_prime = primes_cache[number] ||= is_prime(number)
+        break unless is_prime
         n += 1
         max_coefficients
       end
