29af3e9d1556ea0c9cdb00ade677f48bb39fc895
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-04 16:24:37 -0500

    add count_sums method and corresponding tests for coin combination calculations

---
+++ project_euler/count_sums.rb
@@ -0,0 +1,12 @@
+def count_sums(target, coins)
+  ways = Array.new(target + 1, 0)
+  ways[0] = 1
+
+  coins.each do |coin|
+    (coin..target).each do |amount|
+      ways[amount] += ways[amount - coin]
+    end
+  end
+
+  ways[target]
+end
---
+++ tests/count_sums_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require_relative "../project_euler/count_sums"
+
+class CountSumsTest < Minitest::Test
+  def test_count_sums_with_multiple_ways
+    target = 5
+    coins = [1, 2, 5]
+    assert_equal 4, count_sums(target, coins)
+  end
+
+  def test_count_sums_with_multiple_ways_for_large_target
+    target = 200
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    assert_equal 73682, count_sums(target, coins)
+  end
+end
