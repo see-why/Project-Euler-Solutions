be65891fee9ca3188810220dfaf92f3c319d1abd
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-05 23:17:23 -0500

    remove redundant tests for known cycles in reciprocal_cycles

--- tests/reciprocal_cycles_test.rb
+++ tests/reciprocal_cycles_test.rb
@@ -6,11 +6,6 @@ class ReciprocalCyclesTest < Minitest::Test
     assert_equal 7, reciprocal_cycles(10)
   end
 
-  def test_known_cycles
-    assert_equal 6, find_recurring_cycle("142857142857")
-    assert_equal 1, find_recurring_cycle("333333333333")
-  end
-
   def test_full_problem
     assert_equal 983, reciprocal_cycles(1000)
   end
