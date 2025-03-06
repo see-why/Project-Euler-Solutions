5a2bf9b6b9001f88e0cf864db4bc9b63f92a567c
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-05 23:18:10 -0500

    add SimpleCov for test coverage tracking

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,5 +1,11 @@
 require "minitest/autorun"
 require "minitest/pride"  # For colored output
+require "simplecov"
+
+SimpleCov.start do
+  add_filter "/tests/"
+  track_files "project_euler/**/*.rb"
+end
 
 # Automatically require all test files
 Dir.glob("./tests/**/*_test.rb").each { |file| require file }
