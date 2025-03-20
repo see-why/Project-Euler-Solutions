e095be609c98bc7c7e56ea22b07a902a7c1bcdd2
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-19 23:56:51 -0400

    add test for truncatable primes sum starting from a custom value

--- tests/truncatable_primes_test.rb
+++ tests/truncatable_primes_test.rb
@@ -7,4 +7,8 @@ class TruncatablePrimesTest < Minitest::Test
   def test_truncatable_primes_sum
     assert_equal 748_317, truncatable_primes
   end
+
+  def test_truncatable_primes_sum_from_three_hundred_and_seventy_nine
+    assert_equal 748_317, truncatable_primes(3797)
+  end
 end
