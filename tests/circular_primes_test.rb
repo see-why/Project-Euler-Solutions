6a095ada687b6871966cd3dbab71d66d14ce237e
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-23 23:41:36 -0400

    add test case for circular primes with upper bound of one hundred thousand

--- tests/circular_primes_test.rb
+++ tests/circular_primes_test.rb
@@ -4,12 +4,16 @@ require 'minitest/autorun'
 require_relative '../project_euler/circular_primes'
 
 class CircularPrimesTest < Minitest::Test
+  def test_something_with_upper_bound_ten
+    assert_equal 4, circular_primes(10)
+  end
+
   def test_something_with_upper_bound_one_hundred
     assert_equal 13, circular_primes(100)
   end
 
-  def test_something_with_upper_bound_ten
-    assert_equal 4, circular_primes(10)
+  def test_something_with_upper_bound_one_hundred_thousand
+    assert_equal 43, circular_primes(100_000)
   end
 
   def test_something_with_upper_bound_one_million
