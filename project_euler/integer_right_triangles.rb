19cbc568b00293a9ac84c85b48710f7543a52123
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-24 23:36:25 -0400

    refactor integer_right_triangles method to accept upper_bound parameter and simplify loops

--- project_euler/integer_right_triangles.rb
+++ project_euler/integer_right_triangles.rb
@@ -1,21 +1,14 @@
 # frozen_string_literal: true
 
-def integer_right_triangles
+def integer_right_triangles(upper_bound = 1000)
   solutions_map = {}
-  abc_map = {}
-  upper_bound = Math.sqrt(1_000_000).ceil
-  upper_bound.ceil.downto(1).each do |c|
-    upper_bound.ceil.downto(1).each do |b|
+
+  (1..upper_bound / 3).each do |c|
+    (c..upper_bound / 2).each do |b|
       sum = (b**2) + (c**2)
       a = Math.sqrt(sum)
       next unless (a % 1).zero?
       next if a + b + c > 1_000
-
-      abc_key = [a, b, c].sort.join.to_s
-      puts "abc_key: #{abc_key}"
-      next if abc_map[abc_key]
-
-      abc_map[abc_key] = [a, b, c]
 
       solutions_map[a + b + c] = 0 if solutions_map[a + b + c].nil?
       solutions_map[a + b + c] += 1
