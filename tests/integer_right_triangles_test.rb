ef3af8f7996bb57aec2a1e0e447313add4a0891e
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-24 23:40:04 -0400

    add tests for integer_right_triangles with upper bound scenarios

--- tests/integer_right_triangles_test.rb
+++ tests/integer_right_triangles_test.rb
@@ -5,4 +5,12 @@ class IntegerRightTrianglesTest < Minitest::Test
   def test_integer_right_triangles
     assert_equal 840, integer_right_triangles
   end
+
+  def test_integer_right_triangles_upper_bound_one_hundred
+    assert_equal 90, integer_right_triangles(100)
+  end
+
+  def test_integer_right_triangles_upper_bound_one_hundred_and_thirty
+    assert_equal 120, integer_right_triangles(130)
+  end
 end
