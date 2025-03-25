afec2e735622543129724dfe66f188f31bf71374
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-24 22:55:14 -0400

    add implementation for finding integer right triangles with a perimeter limit

---
+++ project_euler/integer_right_triangles.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+def something
+  solutions_map = {}
+  abc_map = {}
+  upper_bound = Math.sqrt(1_000_000).ceil
+  upper_bound.ceil.downto(1).each do |c|
+    upper_bound.ceil.downto(1).each do |b|
+      sum = (b**2) + (c**2)
+      a = Math.sqrt(sum)
+      next unless (a % 1).zero?
+      next if a + b + c > 1_000
+
+      abc_key = [a, b, c].sort.join.to_s
+      puts "abc_key: #{abc_key}"
+      next if abc_map[abc_key]
+
+      abc_map[abc_key] = [a, b, c]
+
+      solutions_map[a + b + c] = 0 if solutions_map[a + b + c].nil?
+      solutions_map[a + b + c] += 1
+    end
+  end
+
+  solutions_map.key(solutions_map.values.max)
+end
