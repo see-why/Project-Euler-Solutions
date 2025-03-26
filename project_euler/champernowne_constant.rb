ac0fcad99e1923d1e28acca5ece3a18d0305fd10
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-25 21:58:38 -0400

    add champernowne_constant method to calculate the product of specific digits in its decimal expansion

---
+++ project_euler/champernowne_constant.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+def champernowne_constant
+  product = 1
+  digits = []
+
+  (1..1_000_000).each do |n|
+    digits << n.to_s
+  end
+  irrational_decimal_fraction = digits.join
+
+  (0..6).each do |n|
+    index = 10**n
+    product *= irrational_decimal_fraction[index - 1].to_i
+  end
+
+  product
+end
+
+puts champernowne_constant
