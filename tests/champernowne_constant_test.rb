5a8e69f6d8c086b164a7bb971d5df5db76c71c11
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-03-25 21:58:42 -0400

    add tests for champernowne_constant method

---
+++ tests/champernowne_constant_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative '../project_euler/champernowne_constant'
+
+class ChampernowneConstantTest < Minitest::Test
+  def test_champernowne_constant
+    assert_equal 210, champernowne_constant
+  end
+end
