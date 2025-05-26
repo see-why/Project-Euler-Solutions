90035b8e3d5a86ea61de40d4452fd7c6de5b8807
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-25 21:17:19 -0400

    refactor large_sum_test for consistency and readability

--- tests/large_sum_test.rb
+++ tests/large_sum_test.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 # frozen_string_literal
 
 require 'minitest/autorun'
-require_relative '../project_euler/large_sum.rb'
+require_relative '../project_euler/large_sum'
 
 class LargeSumTest < Minitest::Test
   def test_one_million
-    assert_equal 1, large_sum([1,0,0,0,0,0,0,0])
+    assert_equal 1, large_sum([1, 0, 0, 0, 0, 0, 0, 0])
   end
 
   def test_large_array
-    assert_equal 12440, large_sum([1, 1, 1, 1, 1, 11, 1, 12, 23, 24, 12345, 19])
+    assert_equal 12_440, large_sum([1, 1, 1, 1, 1, 11, 1, 12, 23, 24, 12_345, 19])
   end
 end
