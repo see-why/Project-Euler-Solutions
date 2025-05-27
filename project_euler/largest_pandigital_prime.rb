74dabf26ba8a669562b407bf370baab306b56940
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-27 00:18:52 -0400

    fix largest_pandigital_prime method to use correct lower bound and improve permutation logic

--- project_euler/largest_pandigital_prime.rb
+++ project_euler/largest_pandigital_prime.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
+require_relative 'largest_prime_factor'
+
 def largest_pandigital_prime
   upper_bound = 9
-  lower_bound = 0
+  lower_bound = 1
   prime = 2
 
   loop do
-    permutations = (lower_bound..upper_bound).to_a.permutation.map { |perm| perm.join.to_i }
-    prime = permutations.select { |perm| is_prime? perm }.max
-
+    permutations = (lower_bound..upper_bound).to_a.permutation(upper_bound).map { |perm| perm.join.to_i }
+    prime = permutations.select { |perm| prime? perm }.max
+    puts prime
     return prime if prime
 
     upper_bound -= 1
+    puts
   end
 
   prime
 end
+
+puts largest_pandigital_prime
