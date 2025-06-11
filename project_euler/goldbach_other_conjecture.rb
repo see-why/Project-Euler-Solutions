104763b4f10db4f1b0834af01022656029f2f3c4
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-06-11 00:19:52 -0400

    refactor goldbach_other_conjecture for improved logic and performance; optimize prime checks and simplify method parameters

--- project_euler/goldbach_other_conjecture.rb
+++ project_euler/goldbach_other_conjecture.rb
@@ -3,15 +3,17 @@
 require_relative 'largest_prime_factor'
 
 def goldbach_other_conjecture
+  upper_limit = 6_000
   lower_bound = 33
+  primes = primes_less_than(upper_limit)
 
   loop do
-    if prime?(lower_bound)
+    if primes.include?(lower_bound)
       lower_bound += 2 # odd numbers
       next
     end
 
-    break unless goldbach_conjecture? lower_bound
+    break unless goldbach_conjecture?(primes, lower_bound)
 
     lower_bound += 2
   end
@@ -19,16 +21,19 @@ def goldbach_other_conjecture
   lower_bound
 end
 
-def goldbach_conjecture?(num)
-  primes = primes_less_than(num)
-  primes.any? do |prime_factor|
+def goldbach_conjecture?(primes, num)
+  lesser_primes = primes.select { |p| p < num }
+  lesser_primes.any? do |prime_factor|
     n = Math.sqrt((num - prime_factor) / 2)
     n == n.to_i
   end
 end
 
 def primes_less_than(num)
-  (2...num).to_a.map do |val|
-    val if prime?(val)
-  end.compact
+  prime_set = Set.new
+  (2...num).each do |val|
+    prime_set.add val if prime?(val)
+  end
+
+  prime_set.to_a
 end
