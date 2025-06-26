7739353b54438c8c0497abbf4edb09c1c90c9836
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-06-26 00:38:02 -0400

    add additional tests for distinct_primes_factors method to verify functionality with consecutive numbers

--- tests/distinct_primes_factors_test.rb
+++ tests/distinct_primes_factors_test.rb
@@ -5,5 +5,16 @@
 require 'minitest/autorun'
 require_relative '../project_euler/distinct_primes_factors'
 
-class DistinctPrimesFactorsTest
+class DistinctPrimesFactorsTest < Minitest::Test
+  def test_two_consecutive_numbers
+    assert_equal [14, 15], distinct_primes_factors(2, 10)
+  end
+
+  def test_three_consecutive_numbers
+    assert_equal [644, 645, 646], distinct_primes_factors(3, 600)
+  end
+
+  def test_four_consecutive_numbers
+    assert_equal [134_043, 134_044, 134_045, 134_046], distinct_primes_factors(4, 100_000)
+  end
 end
