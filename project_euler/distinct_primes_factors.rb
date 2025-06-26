8c94ca0a85f1cee0f457d54138adc878870a98ec
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-06-25 23:50:04 -0400

    add distinct_primes_factors method stub to explore consecutive numbers with distinct prime factors

---
+++ project_euler/distinct_primes_factors.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# first two consecutive numbers to have two distinct prime factors are:
+# 14 = 2 * 7
+# 15 = 3 * 5
+# first three consecutive numbers to have three distinct prime factor are:
+# 644 = 2**2 * 7 * 23
+# 645 = 3 * 5 * 43
+# 646 = 2 * 17 * 19
+# trying to find the first four consecutive numbers to have four distinct
+# prime factors
+def distinct_primes_factors
+
+end
