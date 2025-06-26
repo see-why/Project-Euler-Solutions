6136fbe36a46b3d6e9eb0aaf1e84cc6523727b44
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-06-26 00:37:57 -0400

    implement distinct_primes_factors method to find consecutive numbers with distinct prime factors; add helper methods for prime factorization and caching

--- project_euler/distinct_primes_factors.rb
+++ project_euler/distinct_primes_factors.rb
@@ -9,4 +9,52 @@
 # 646 = 2 * 17 * 19
 # trying to find the first four consecutive numbers to have four distinct
 # prime factors
-def distinct_primes_factors; end
+require 'set'
+require_relative '../project_euler/largest_prime_factor'
+
+def distinct_primes_factors(consecutive_size, start)
+  result = []
+  count = 0
+  primes = Hash.new(0)
+
+  loop do
+    break if count == consecutive_size
+
+    if prime_factors(start, primes).size == consecutive_size
+      result << start
+      count += 1
+    else
+      result = []
+      count = 0
+    end
+
+    start += 1
+  end
+
+  result
+end
+
+def prime_factors(num, primes = {})
+  result = []
+
+  (2..Math.sqrt(num)).each do |val|
+    next unless (num % val).zero?
+
+    result << val if find_or_save(val, primes)
+    quotient = (num / val)
+    result << quotient if find_or_save(quotient, primes)
+  end
+
+  result
+end
+
+def find_or_save(num, primes)
+  return true if primes.key? num
+
+  if prime? num
+    primes[num] += 1
+    return true
+  end
+
+  false
+end
