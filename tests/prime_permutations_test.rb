56a06c5af4b13d797b7e707ef305b37f2817e3da
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-06-29 00:18:07 -0400

    add test for prime_permutations method to verify expected output

--- tests/prime_permutations_test.rb
+++ tests/prime_permutations_test.rb
@@ -4,4 +4,7 @@ require 'minitest/autorun'
 require_relative '../project_euler/prime_permutations'
 
 class PrimePermutationsTest < Minitest::Test
+  def test_prime_permutations
+    assert_equal 296_962_999_629, prime_permutations
+  end
 end
