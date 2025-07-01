3908d8620f2ab2e9225fb6be08c0d767a520b227
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-07-01 01:22:47 -0400

    fix test cases for consecutive_prime_sum to remove unnecessary argument

--- tests/consecutive_prime_sum_test.rb
+++ tests/consecutive_prime_sum_test.rb
@@ -5,6 +5,14 @@ require_relative '../project_euler/consecutive_prime_sum'
 
 class TestConsecutivePrimeSum < Minitest::Test
   def test_for_sum_up_to_fifty
-    assert_equal 41, consecutive_prime_sum(1, 50)
+    assert_equal 41, consecutive_prime_sum(50)
+  end
+
+  def test_for_sum_up_to_one_thousand
+    assert_equal 953, consecutive_prime_sum(1_000)
+  end
+
+  def test_for_sum_up_to_one_million
+    assert_equal 997_651, consecutive_prime_sum(1_000_000)
   end
 end
